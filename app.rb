83f2e14f291e971094c3f965975c0e1ab19285ee
ShotaTsuru <syota.crane@gmail.com>
2021-07-23 14:13:01 +0900

    viewファイルの配置

--- app.rb
+++ app.rb
@@ -4,3 +4,27 @@ require 'sinatra/reloader'
 get '/' do
   'fine!'
 end
+
+get '/path/to' do
+  "this is [/path/to]"
+end
+
+# get '/memo/*' do |name|
+#   "hello #{name}. how are you?"
+# end
+
+get '/memo' do
+  erb :index
+end
+
+get '/memo/new' do
+  erb :new
+end
+
+get '/memo/*/edit' do
+  erb :edit
+end
+
+# get '/memo/*/edit' do |name|
+#     "hello #{name}. how are you?"
+# end
