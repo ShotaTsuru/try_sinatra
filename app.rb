2f52ccb85bc072dc3abad18ab747eff5384d69b9
ShotaTsuru <syota.crane@gmail.com>
2021-07-24 14:57:27 +0900

    保存処理

--- app.rb
+++ app.rb
@@ -8,31 +8,50 @@ require 'json'
 
 class Memo
   attr_accessor :id, :text, :title
-
 end
 
+# topページへ
 get '/memo' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read, object_class: Memo)
   @memos = memos.parse
 
   erb :index
 end
-
+# 新規投稿ページへ
 get '/memo/new' do
   erb :new
 end
 
+# 投稿の保存処理
+post '/memo' do
+  memos = JSON::Parser.new(File.open('./data/sample.json').read)
+  memos_a = memos.parse
+  params[:id] = memos_a[-1]["id"] + 1
+  memos_a << params
+  File.open('./data/sample.json', "w") do |f|
+    JSON.dump(memos_a, f)
+  end
+  redirect to('/memo')
+end
+
+# 詳細ページへ
+get '/memo/*' do
+  erb :show
+end
+
+# 削除処理
+delete '/memo/*/' do
+  redirect to('/memo')
+end
+
+# 編集ページへ
 get '/memo/*/edit' do
   erb :edit
 end
 
-# patch '/memo/*/edit' do
-#   erb :index
-# end
-
-post '/memo' do
-  # 投稿の処理
-  erb :index
+# 編集処理
+patch '/memo/*' do
+  redirect to('/memo')
 end
 
 # get '/memo/*/edit' do |name|
