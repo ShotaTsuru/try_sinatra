c13d50b1c7873c14913f341a3118ccc685a52fe0
ShotaTsuru <syota.crane@gmail.com>
2021-07-25 16:03:38 +0900

    削除機能実装

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ end
 # 投稿の保存処理
 post '/memo' do
   memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  params[:id] = memos_a[-1]["id"] + 1
+  params[:id] = (memos_a[-1]["id"].to_i + 1).to_s
   memos_a << params
   File.open('./data/sample.json', "w") do |f|
     JSON.dump(memos_a, f)
@@ -42,20 +42,25 @@ end
 # 詳細ページへ
 get '/memo/:id' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memo = memos.select {|n| n["id"] == params[:id].to_i}
+  memo = memos.select {|n| n["id"] == params[:id]}
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :show
 end
 
 # 削除処理
-delete '/memo/*/' do
+delete '/memo/:id' do
+  memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
+  memos_a = memos.delete_if {|n| n["id"] == params[:id]}
+  File.open('./data/sample.json', "w") do |f|
+    JSON.dump(memos_a, f)
+  end
   redirect to('/memo')
 end
 
 # 編集ページへ
 get '/memo/:id/edit' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memo = memos.select {|n| n["id"] == params[:id].to_i}
+  memo = memos.select {|n| n["id"] == params[:id]}
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :edit
 end
@@ -63,8 +68,7 @@ end
 # 編集処理
 patch '/memo/:id' do
   memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memos_a.map!{|a| a["id"] == params["id"].to_i ? params : a}
-  binding.irb
+  memos_a.map!{|a| a["id"] == params["id"] ? params : a}
   File.open('./data/sample.json', "w") do |f|
     JSON.dump(memos_a, f)
   end
