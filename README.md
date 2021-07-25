f508a56b5d177231f14a6748add40960bc7d0aee
ShotaTsuru <syota.crane@gmail.com>
2021-07-25 17:29:29 +0900

    READMEの編集

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # try_sinatra
+try_sinatraは簡単なメモアプリ
 
-# sinatraのミニアプリ作成
+# DEMO
+投稿
+https://i.gyazo.com/a0bce0a0d97e3b145e6e4512af716e82.gif
+削除
+https://i.gyazo.com/ce5122bb2530033cc3fc1d434cb7fef0.gif
+編集
+https://i.gyazo.com/f4216b3587eed93ce958f0b92ef898a7.gif
+# Features
+最低限必要なものを実装。
+・XSS対策済み。
+・CSSは黒と白でシンプルに
+
+# Requirement
+
+
+# Usage
+```bash
+#任意のフォルダに
+git clone 
+#cloneしたフォルダへ移動
+cd try_sinatra
+#sinatraサーバーの立ち上げ
+bundle exec ruby app.rb
+# http://localhost:4567/memo へアクセス
+```
+あとはメモとしてお使いください。
+
+# Note
+フィヨルドブートキャンプの課題
+
+# Author
+
+作成者　鶴 翔太
--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'cgi/escape'
 
-# get '/memo/*' do |name|
-#   "hello #{name}. how are you?"
-# end
-
 class Memo
   attr_accessor :id, :text, :title
+
   def []=(key, value)
     instance_variable_set("@#{key}", value)
   end
@@ -18,6 +17,10 @@ helpers do
   def escape(text)
     CGI.escape_html(text)
   end
+end
+
+error 404 do
+  status 404
 end
 
 # topページへ
@@ -30,56 +33,46 @@ end
 get '/memo/new' do
   erb :new
 end
-
 # 投稿の保存処理
 post '/memo' do
   memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  params[:id] = (memos_a[-1]["id"].to_i + 1).to_s
+  params[:id] = (memos_a[-1]['id'].to_i + 1).to_s
   user_input_escape(params)
-  binding.irb
   memos_a << params
-  File.open('./data/sample.json', "w") do |f|
+  File.open('./data/sample.json', 'w') do |f|
     JSON.dump(memos_a, f)
   end
   redirect to('/memo')
 end
-
 # 詳細ページへ
 get '/memo/:id' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memo = memos.select {|n| n["id"] == params[:id]}
+  memo = memos.select { |n| n['id'] == params[:id] }
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :show
 end
-
 # 削除処理
 delete '/memo/:id' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memos_a = memos.delete_if {|n| n["id"] == params[:id]}
-  File.open('./data/sample.json', "w") do |f|
+  memos_a = memos.delete_if { |n| n['id'] == params[:id] }
+  File.open('./data/sample.json', 'w') do |f|
     JSON.dump(memos_a, f)
   end
   redirect to('/memo')
 end
-
 # 編集ページへ
 get '/memo/:id/edit' do
   memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memo = memos.select {|n| n["id"] == params[:id]}
+  memo = memos.select { |n| n['id'] == params[:id] }
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :edit
 end
-
 # 編集処理
 patch '/memo/:id' do
   memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memos_a.map!{|a| a["id"] == params["id"] ? params : a}
-  File.open('./data/sample.json', "w") do |f|
+  memos_a.map! { |a| a['id'] == params['id'] ? params : a }
+  File.open('./data/sample.json', 'w') do |f|
     JSON.dump(memos_a, f)
   end
   redirect to('/memo')
 end
-
-# get '/memo/*/edit' do |name|
-#     "hello #{name}. how are you?"
-# end
