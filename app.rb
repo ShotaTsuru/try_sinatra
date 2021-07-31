307992412c5373687a860c0a0aa3dc21e0ff2b99
ShotaTsuru <syota.crane@gmail.com>
2021-07-31 11:03:41 +0900

    jsonファイルdataの初期化とundefinemethodの修正

--- app.rb
+++ app.rb
@@ -36,8 +36,11 @@ end
 # 投稿の保存処理
 post '/memo' do
   memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  params[:id] = (memos_a[-1]['id'].to_i + 1).to_s
-  user_input_escape(params)
+  if memos_a.empty?
+    params[:id] = "1"
+  else
+    params[:id] = (memos_a[-1]['id'].to_i + 1).to_s
+  end
   memos_a << params
   File.open('./data/sample.json', 'w') do |f|
     JSON.dump(memos_a, f)
