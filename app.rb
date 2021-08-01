fef3e5d715ad5d54f621d2fad24341295a8d3e31
ShotaTsuru <syota.crane@gmail.com>
2021-08-01 12:58:17 +0900

    jsonファイル名の修正、破壊的メソッドdelete_ifの変数削除、ハンガリアン記法memos_aの修正 Please enter the commit message for your changes. Lines starting

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ end
 
 # topページへ
 get '/memo' do
-  memos = JSON::Parser.new(File.open('./data/sample.json').read, object_class: Memo)
-  @memos = memos.parse
+  parser = JSON::Parser.new(File.open('./data/database_file.json').read, object_class: Memo)
+  @memos = parser.parse
   erb :index
 end
 
@@ -37,22 +37,22 @@ end
 
 # 投稿の保存処理
 post '/memo' do
-  memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  params[:id] = if memos_a.empty?
+  memos = JSON::Parser.new(File.open('./data/database_file.json').read).parse
+  params[:id] = if memos.empty?
                   '1'
                 else
-                  (memos_a[-1]['id'].to_i + 1).to_s
+                  (memos[-1]['id'].to_i + 1).to_s
                 end
-  memos_a << params
-  File.open('./data/sample.json', 'w') do |f|
-    JSON.dump(memos_a, f)
+  memos << params
+  File.open('./data/database_file.json', 'w') do |f|
+    JSON.dump(memos, f)
   end
   redirect to('/memo')
 end
 
 # 詳細ページへ
 get '/memo/:id' do
-  memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
+  memos = JSON::Parser.new(File.open('./data/database_file.json').read).parse
   memo = memos.select { |n| n['id'] == params[:id] }
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :show
@@ -60,17 +60,17 @@ end
 
 # 削除処理
 delete '/memo/:id' do
-  memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memos_a = memos.delete_if { |n| n['id'] == params[:id] }
-  File.open('./data/sample.json', 'w') do |f|
-    JSON.dump(memos_a, f)
+  memos = JSON::Parser.new(File.open('./data/database_file.json').read).parse
+  memos.delete_if { |n| n['id'] == params[:id] }
+  File.open('./data/database_file.json', 'w') do |f|
+    JSON.dump(memos, f)
   end
   redirect to('/memo')
 end
 
 # 編集ページへ
 get '/memo/:id/edit' do
-  memos = JSON::Parser.new(File.open('./data/sample.json').read).parse
+  memos = JSON::Parser.new(File.open('./data/database_file.json').read).parse
   memo = memos.select { |n| n['id'] == params[:id] }
   @memo = JSON.parse(memo.to_json, object_class: Memo)[0]
   erb :edit
@@ -78,10 +78,10 @@ end
 
 # 編集処理
 patch '/memo/:id' do
-  memos_a = JSON::Parser.new(File.open('./data/sample.json').read).parse
-  memos_a.map! { |a| a['id'] == params['id'] ? params : a }
-  File.open('./data/sample.json', 'w') do |f|
-    JSON.dump(memos_a, f)
+  memos = JSON::Parser.new(File.open('./data/database_file.json').read).parse
+  memos.map! { |a| a['id'] == params['id'] ? params : a }
+  File.open('./data/database_file.json', 'w') do |f|
+    JSON.dump(memos, f)
   end
   redirect to('/memo')
 end
